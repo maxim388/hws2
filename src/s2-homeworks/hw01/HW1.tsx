20147e2e57a1c0d9811ce7d89e27b5848f7a114c
Maxim Alenchikov <maxim.alenchikov@gmail.com>
2023-06-29 22:14:44 +0300

    samurai

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -7,10 +7,10 @@ import avatar from './avatar.png'
 
 /*
 * 1 - описать тип MessageType + 
-* 2 - описать тип MessagePropsType в файле Message.tsx
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
-* 5 - сделать стили в соответствии с дизайном
+* 2 - описать тип MessagePropsType в файле Message.tsx +
+* 3 - в файле Message.tsx отобразить приходящие данные +
+* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx +
+* 5 - сделать стили в соответствии с дизайном +
 * */
 
 // нужно создать правильный тип вместо any
@@ -28,16 +28,16 @@ export type MessageType = {
 
 // структуру объекта не менять
 export const message0: MessageType = {
-    id: 0,
-    user: {
-        avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
-    },
-    message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
-        time: '22:00', // можно менять
-    },
-}
+  id: 0,
+  user: {
+    avatar: avatar, // можно менять
+    name: "Some Name", // можно менять
+  },
+  message: {
+    text: "some textsome textsome textsome textsome textsome textsome textsome textsome textsome textsome textsome textsome textsome textsome textsome textsome textsome", // можно менять
+    time: "22:00", // можно менять
+  },
+};
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
