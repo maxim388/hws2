e2708664662f6da4f2b8f1246fe52cc00373f58a
Maxim Alenchikov <maxim.alenchikov@gmail.com>
2023-08-08 13:44:52 +0300

    hw2

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -4,41 +4,34 @@ import { MessageType } from '../HW1'
 
 // нужно создать правильный тип вместо any
 export type MessagePropsType = {
-    massage:  MessageType,
+    message:  MessageType,
 }
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
-    debugger
+    // debugger
     return (
-        <div id={'hw1-message-' + props.message.id} className={s.message}>
-            <div className={s.imageAndText}>
-                <img
-                    id={'hw1-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
-                />
-                <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </div>
-                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </pre>
-                </div>
+      <div id={"hw1-message-" + props.message.id} className={s.message}>
+        <div className={s.imageAndText}>
+          <img
+            id={"hw1-avatar-" + props.message.id}
+            src={props.message.user.avatar}
+            alt="avatar"
+          />
+          <div className={s.text}>
+            <div id={"hw1-name-" + props.message.id} className={s.name}>
+              {props.message.user.name}
             </div>
-            <div id={'hw1-time-' + props.message.id} className={s.time}>
-                {/*создаёт студент*/}
-
-                {/**/}
-            </div>
+            <pre id={"hw1-text-" + props.message.id} className={s.messageText}>
+              {props.message.message.text}
+            </pre>
+          </div>
         </div>
-    )
+        <div id={"hw1-time-" + props.message.id} className={s.time}>
+          {props.message.message.time}
+        </div>
+      </div>
+    );
 }
 
 export default Message
